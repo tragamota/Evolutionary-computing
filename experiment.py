5b9b32064e788c9bddab7893185495a622628d31
Nickname5862 <nick.schuitemaker@hotmail.nl>
2023-03-02 17:27:00 +0100

    majority of the plots finished. Finishing the last is childs-play, just have to go now

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ import random
 import time
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from fitness import CountingOne, Trap
 from crossover import UniformCrossover, TwoPointCrossover
@@ -28,21 +29,38 @@ def best_fitness(population, fitness):
 
 
 def run_generation(population_size, solution_length, fitness, mutation):
+    global selection_decisions
+
     current_population = Population(population_size, solution_length, generation=0)
     generation_info = [0, best_fitness(current_population, fitness)]  # (best gen, best fitness)
 
     generations = []
 
     while not stopping_criteria(current_population, generation_info):
-        # one generation:
-        random.shuffle(current_population)
-        parents = list(zip(current_population[:-1:2], current_population[1::2]))
-        families = [(*p, *mutation.mutate(*p)) for p in parents]
 
-        # fight to the death
+        # one generation:
+        random.shuffle(current_population) # 1. shuffle
+        parents = list(zip(current_population[:-1:2], current_population[1::2])) # 2. pairwise matching
+        families = [(*p, *mutation.mutate(*p)) for p in parents] # 3. family competition
+        _next_pop = [sorted(f, key=fitness.score)[-2:] for f in families] # 4. fight to the death
         # Note: `sorted()` sorts in-order, meaning children will remain at the end of the list
-        next_pop = [sorted(f, key=fitness.score)[-2:] for f in families]
-        next_pop = [item for sublist in next_pop for item in sublist]
+        next_pop = [item for sublist in _next_pop for item in sublist]
+
+        # Here we check:
+        # given the families:
+        # 1. are at the parents any two bits different?
+        # 2. at those indices, did both who won have the same bit-value?
+        # 3. if so, add to the respective summation. We just want to plot the absolute number.
+        #    we only want to compare the two. No need to average or something
+        for i in range(len(families)): # a four-tuple (parent1, parent2, child1, child2)
+            par1 = families[i][0]
+            par2 = families[i][1]
+            win1 = _next_pop[i][0]
+            win2 = _next_pop[i][1]
+            for j in range(len(par1.x)):
+                if par1[j] != par2[j] and win1[j] == win2[j]:
+                    selection_decisions[win1[j]] += 1
+        # TODO NOW DO IT BASED ON GENERATIONS!!
 
         next_generation = Population(population_size, solution_length,
                                      generation=current_population.generation + 1, x=next_pop)
@@ -136,66 +154,121 @@ def run_experiment(fitness, mutation, population_size=10, max_population_size=12
     return True, np.sum(successful_runs), current_population_size, generation_count, (end_time - starting_time) / 20
 
 
+
+def plotMeasures():
+
+    global selection_decisions
+    selection_decisions = [0,0] # TODO
+
+    N = 200
+    l = 40
+    fitness = CountingOne()
+    mutation = TwoPointCrossover()
+    # "a single run is sufficient"
+    _, _, generations = run_generation(N, l, fitness, mutation)
+
+    gen_range = range(0, len(generations))
+    # 1. "Plot the proportion prop(t) of bits-1 in the entire population as a function of the generation t."
+    prop_1_bits = [np.average([sum(sol.x) / len(sol.x) for sol in gen]) for gen in generations]
+    # 2. "Plot the number of selection errors Err(t) and the number of correct selection decisions"
+    selection_errors = selection_decisions[0]
+    correct_selection = selection_decisions[1]
+    print(selection_errors)
+    print(correct_selection)
+    # 3. "Plot the number of solutions in the population that are member of the respective schemata"
+    no_1_schemata = [np.sum([1 for sol in gen if sol.x[0] == 1]) / N for gen in generations]
+    fitni_1_schemata = [[fitness.score(sol) for sol in gen if sol.x[0] == 1] for gen in generations]
+    fitni_0_schemata = [[fitness.score(sol) for sol in gen if sol.x[0] == 0] for gen in generations]
+    avg_fitness_1_schemata = [np.average(fitness) for fitness in fitni_1_schemata]
+    avg_fitness_0_schemata = [np.average(fitness) for fitness in fitni_0_schemata]
+    std_fitness_1_schemata = [np.std(fitness) for fitness in fitni_1_schemata]
+    std_fitness_0_schemata = [np.std(fitness) for fitness in fitni_0_schemata]
+
+    ax = plt.subplot()
+    ax.plot(gen_range, prop_1_bits)
+    ax.set_xlabel("generation")
+    ax.set_ylabel("proportion of 1-bits in population")
+    ax.set_xlim(0, len(generations)-1)
+    ax.set_ylim((0,1))
+    plt.show()
+
+    _, axes = plt.subplots(1, 2)
+    ax = axes[0]
+    ax.plot(gen_range, no_1_schemata)
+    ax.plot(gen_range, [1 - i for i in no_1_schemata])
+    ax.set_xlabel("generation")
+    ax.set_ylabel("proportion of solutions part of schemata")
+    ax.set_xlim(0, len(generations)-1)
+    ax.set_ylim(0, 1)
+    ax.legend(["1**..** schemata", "0**..** schemata"])
+    ax = axes[1]
+    # ax.plot(gen_range, avg_fitness_1_schemata)
+    plt.errorbar(x=gen_range, y=avg_fitness_1_schemata, yerr=std_fitness_1_schemata)
+    # ax.plot(gen_range, avg_fitness_0_schemata)
+    plt.errorbar(x=[x+0.1 for x in gen_range], y=avg_fitness_0_schemata, yerr=std_fitness_0_schemata)
+    # small offset in x, such that standard deviations don't overlap
+    ax.set_xlabel("generation")
+    ax.set_ylabel("average fitness of schemata")
+    ax.legend(["1**..** schemata", "0**..** schemata"])
+    ax.set_xlim(0, len(gen_range)-1)
+    plt.show()
+
+
 if __name__ == "__main__":
+
+    plotMeasures()
+    quit()
+
+
 
     print(f"Counting ones and Uniform crossover")
     fitness = CountingOne()
     mutation = UniformCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Counting ones and Two point crossover")
     fitness = CountingOne()
     mutation = TwoPointCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap deceptive tightly linked and Uniform crossover")
     fitness = Trap(k=4, d=1, tightly_linked=True)
     mutation = UniformCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap deceptive tightly linked and Two point crossover")
     fitness = Trap(k=4, d=1, tightly_linked=True)
     mutation = TwoPointCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap non-deceptive tightly linked and Uniform crossover")
     fitness = Trap(k=4, d=2.5, tightly_linked=True)
     mutation = UniformCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap non-deceptive tightly linked and Two point crossover")
     fitness = Trap(k=4, d=2.5, tightly_linked=True)
     mutation = TwoPointCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap deceptive not tightly linked and Uniform crossover")
     fitness = Trap(k=4, d=1, tightly_linked=False)
     mutation = UniformCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap deceptive not tightly linked and Two point crossover")
     fitness =  Trap(k=4, d=1, tightly_linked=False)
     mutation = TwoPointCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap non-deceptive not tightly linked and Uniform crossover")
     fitness = Trap(k=4, d=2.5, tightly_linked=False)
     mutation = UniformCrossover()
-
     print(run_experiment(fitness, mutation))
 
     print(f"Trap non-deceptive not tightly linked and Two point crossover")
     fitness = Trap(k=4, d=2.5, tightly_linked=False)
     mutation = TwoPointCrossover()
-
     print(run_experiment(fitness, mutation))
 
 
